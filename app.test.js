68cbd2d4665741c172995a69866bff60638c0556
daanadriaan <daan@daanadriaan.nl>
2025-10-23 12:07:56 +0200

    Les 5: Test toegevoegd

---
+++ app.test.js
@@ -0,0 +1,8 @@
+test('1 + 1 moet 2 zijn', () => {
+    expect(1 + 1).toBe(2);
+});
+
+// Een test die expres faalt (om te demo-en)
+// test('Dit faalt expres', () => {
+//   expect(true).toBe(false);
+// });
